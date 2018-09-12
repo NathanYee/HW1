80ff0799a4606418ad7ba28341090a6449d2f8af
nathanyee <nathaniel.yee@students.olin.edu>
2018-09-11 21:24:02 -0400

    moved things around to be pretty

--- hw1.t.v
+++ hw1.t.v
@@ -10,16 +10,16 @@ module demorgan_test (); // nA and nB
 
   // Run sequence of test stimuli
   initial begin
-    $display("| A B | ~A ~B | ~A~B  |  A+B  | ~(A+B) | ~A+~B | AB | ~(AB) |");            // Prints header for truth table
-    $display("|_____|_______|_______|_______|________|_______|____|_______|");            
+    $display("| A B | ~A ~B | ~A~B | A+B | AB | ~(A+B) | ~A+~B | ~(AB) |");            // Prints header for truth table
+    $display("|_____|_______|______|_____|____|________|_______|_______|");            
     A=0;B=0; #1                                 // Set A and B, wait for update (#1)
-    $display("| %b %b |  %b  %b |    %b  |    %b  |      %b |     %b |  %B |     %B |", A,B, nA, nB, nAandnB, AorB, npAorB, nAornB, AandB, npAandB);
+    $display("| %b %b |  %b  %b |    %b |   %b |  %B |      %b |     %b |     %B |", A,B, nA, nB, nAandnB, AorB, AandB, npAorB, nAornB, npAandB);
     A=0;B=1; #1                                 // Set A and B, wait for new update
-    $display("| %b %b |  %b  %b |    %b  |    %b  |      %b |     %b |  %B |     %B |", A,B, nA, nB, nAandnB, AorB, npAorB, nAornB, AandB, npAandB);
+    $display("| %b %b |  %b  %b |    %b |   %b |  %B |      %b |     %b |     %B |", A,B, nA, nB, nAandnB, AorB, AandB, npAorB, nAornB, npAandB);
     A=1;B=0; #1
-    $display("| %b %b |  %b  %b |    %b  |    %b  |      %b |     %b |  %B |     %B | ", A,B, nA, nB, nAandnB, AorB, npAorB, nAornB, AandB, npAandB);
+    $display("| %b %b |  %b  %b |    %b |   %b |  %B |      %b |     %b |     %B | ", A,B, nA, nB, nAandnB, AorB, AandB, npAorB, nAornB, npAandB);
     A=1;B=1; #1
-    $display("| %b %b |  %b  %b |    %b  |    %b  |      %b |     %b |  %B |     %B |", A,B, nA, nB, nAandnB, AorB, npAorB, nAornB, AandB, npAandB);
+    $display("| %b %b |  %b  %b |    %b |   %b |  %B |      %b |     %b |     %B |", A,B, nA, nB, nAandnB, AorB, AandB, npAorB, nAornB, npAandB);
   end
 
 endmodule    // End demorgan_test
