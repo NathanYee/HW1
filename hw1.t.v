629425798faebba5b7519e2f7f87387815112e46
nathanyee <nathaniel.yee@students.olin.edu>
2018-09-11 21:04:59 -0400

    now using one module

--- hw1.t.v
+++ hw1.t.v
@@ -1,48 +1,24 @@
 `include "hw1.v"
 
-
-module demorgan_test3 (); // n(A or B)
-
-  // Instantiate device/module under test
-  reg A, B;                // Primary test inputs
-  wire AorB, npAorB;       // Test outputs
-
-  demorgan3 dut(A, B, AorB, npAorB);  // Module to be tested
-
-
-  // Run sequence of test stimuli
-  initial begin
-    $display("A B | A+B | ~(A+B) ");            // Prints header for truth table
-    A=0;B=0; #1                                 // Set A and B, wait for update (#1)
-    $display("%b %b |   %b |      %b  ", A, B, AorB, npAorB);
-    A=0;B=1; #1                                // Set A and B, wait for new update
-    $display("%b %b |   %b |      %b  ", A, B, AorB, npAorB);
-    A=1;B=0; #1
-    $display("%b %b |   %b |      %b  ", A, B, AorB, npAorB);
-    A=1;B=1; #1
-    $display("%b %b |   %b |      %b  ", A, B, AorB, npAorB);
-  end
-endmodule    // End demorgan_test
-
-module demorgan_test4 (); // nA and nB
+module demorgan_test (); // nA and nB
 
   // Instantiate device/module under test
   reg A, B;                // Primary test inputs
-  wire nA, nB, nAandnB;    // Test outputs
+  wire nA, nB, nAandnB, AorB, npAorB;    // Test outputs
 
-  demorgan4 dut(A, B, nA, nB, nAandnB);  // Module to be tested
-
+  demorgan dut(A, B, nA, nB, nAandnB, AorB, npAorB);  // Module to be tested
 
   // Run sequence of test stimuli
   initial begin
-    $display("A B | ~A ~B | ~A~B ");            // Prints header for truth table
+    $display("A B | ~A ~B | ~A~B  |  A+B  | ~(A+B) |");            // Prints header for truth table
     A=0;B=0; #1                                 // Set A and B, wait for update (#1)
-    $display("%b %b |  %b  %b |    %b  ", A,B, nA, nB, nAandnB);
+    $display("%b %b |  %b  %b |    %b  |    %b  |      %b |", A,B, nA, nB, nAandnB, AorB, npAorB);
     A=0;B=1; #1                                 // Set A and B, wait for new update
-    $display("%b %b |  %b  %b |    %b  ", A,B, nA, nB, nAandnB);
+    $display("%b %b |  %b  %b |    %b  |    %b  |      %b |", A,B, nA, nB, nAandnB, AorB, npAorB);
     A=1;B=0; #1
-    $display("%b %b |  %b  %b |    %b  ", A,B, nA, nB, nAandnB);
+    $display("%b %b |  %b  %b |    %b  |    %b  |      %b |", A,B, nA, nB, nAandnB, AorB, npAorB);
     A=1;B=1; #1
-    $display("%b %b |  %b  %b |    %b  ", A,B, nA, nB, nAandnB);
+    $display("%b %b |  %b  %b |    %b  |    %b  |      %b |", A,B, nA, nB, nAandnB, AorB, npAorB);
   end
+
 endmodule    // End demorgan_test
--- hw1.v
+++ hw1.v
@@ -1,25 +1,12 @@
-
-module demorgan3    // nA and nB
+module demorgan       // nA and nB
 (
-  input  A,         // Single bit inputs
+  input  A,           // Single bit inputs
   input  B,
-  output AorB,      // Output intermediate complemented inputs
-  output npAorB     // Single bit output, ~(A+B)
-);
-
-  wire AorB;
-  or orgate(AorB, A, B); // Top or gate takes signal A and B as inputs and produces signal AorB
-  not npAorBinv(npAorB, AorB);  	
-endmodule
-
-
-module demorgan4     // nA and nB
-(
-  input  A,          // Single bit inputs
-  input  B,
-  output nA,         // Output intermediate complemented inputs
+  output nA,          // Output intermediate complemented inputs
   output nB,
-  output nAandnB     // Single bit output, (~A)*(~B)
+  output nAandnB,     // Single bit output, (~A)*(~B)
+  output AorB,
+  output npAorB       // parentheses
 );
 
   wire nA;
@@ -27,5 +14,8 @@ module demorgan4     // nA and nB
   not Ainv(nA, A);  	// Top inverter is named Ainv, takes signal A as input and produces signal nA
   not Binv(nB, B);
   and andgate(nAandnB, nA, nB); 	// AND gate produces nAandnB from nA and nB
+  wire AorB;
+  or orgate(AorB, A, B);
+  not AorBinv(npAorB, AorB);
 
 endmodule
