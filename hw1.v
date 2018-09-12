4f3f3fce00004d0fae28ed1f1eea7ff8ecc4abe7
nathanyee <nathaniel.yee@students.olin.edu>
2018-09-11 21:20:55 -0400

    ~(AB)

--- hw1.v
+++ hw1.v
@@ -7,17 +7,22 @@ module demorgan       // nA and nB
   output nAandnB,     // Single bit output, (~A)*(~B)
   output AorB,
   output npAorB,      // ~(A+B)
-  output nAornB       // ~A+~B
+  output nAornB,      // ~A+~B
+  output AandB,       // AB
+  output npAandB      // ~(AB)
 );
 
   wire nA;
   wire nB;
+  wire AorB;
+  wire AandB;
   not Ainv(nA, A);  	// Top inverter is named Ainv, takes signal A as input and produces signal nA
   not Binv(nB, B);
   and andgate(nAandnB, nA, nB); 	// AND gate produces nAandnB from nA and nB
-  wire AorB;
   or orgate(AorB, A, B);
   not AorBinv(npAorB, AorB);
   or orgate(nAornB, nA, nB);
+  and andgate(AandB, A, B);
+  not AandBinv(npAandB, AandB);
 
 endmodule
