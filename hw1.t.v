787871b6a6351f31f68fe45afb5aee2a623f3a8b
nathanyee <nathaniel.yee@students.olin.edu>
2018-09-11 21:06:32 -0400

    made header pretty

--- hw1.t.v
+++ hw1.t.v
@@ -10,15 +10,16 @@ module demorgan_test (); // nA and nB
 
   // Run sequence of test stimuli
   initial begin
-    $display("A B | ~A ~B | ~A~B  |  A+B  | ~(A+B) |");            // Prints header for truth table
+    $display("| A B | ~A ~B | ~A~B  |  A+B  | ~(A+B) |");            // Prints header for truth table
+    $display("|_____|_______|_______|_______|________|");            
     A=0;B=0; #1                                 // Set A and B, wait for update (#1)
-    $display("%b %b |  %b  %b |    %b  |    %b  |      %b |", A,B, nA, nB, nAandnB, AorB, npAorB);
+    $display("| %b %b |  %b  %b |    %b  |    %b  |      %b |", A,B, nA, nB, nAandnB, AorB, npAorB);
     A=0;B=1; #1                                 // Set A and B, wait for new update
-    $display("%b %b |  %b  %b |    %b  |    %b  |      %b |", A,B, nA, nB, nAandnB, AorB, npAorB);
+    $display("| %b %b |  %b  %b |    %b  |    %b  |      %b |", A,B, nA, nB, nAandnB, AorB, npAorB);
     A=1;B=0; #1
-    $display("%b %b |  %b  %b |    %b  |    %b  |      %b |", A,B, nA, nB, nAandnB, AorB, npAorB);
+    $display("| %b %b |  %b  %b |    %b  |    %b  |      %b |", A,B, nA, nB, nAandnB, AorB, npAorB);
     A=1;B=1; #1
-    $display("%b %b |  %b  %b |    %b  |    %b  |      %b |", A,B, nA, nB, nAandnB, AorB, npAorB);
+    $display("| %b %b |  %b  %b |    %b  |    %b  |      %b |", A,B, nA, nB, nAandnB, AorB, npAorB);
   end
 
 endmodule    // End demorgan_test
